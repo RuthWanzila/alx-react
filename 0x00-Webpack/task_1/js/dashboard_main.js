1db86662badfd7b22d0ace954737c3e001d9c486
RuthWanzila <wanzilaruth533@gmail.com>
2024-05-20 12:23:36 +0000

    alx-react

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Paragraph element: Holberton Dashboard
+$('body').append('<p>Holberton Dashboard</p>');
+
+// Paragraph element: Dashboard data for the students
+$('body').append('<p>Dashboard data for the students</p>');
+
+// Button element with the text Click here to get started
+$('body').append('<button>Click here to get started</button>');
+
+// Paragraph element with id='count'
+$('body').append('<p id="count"></p>');
+
+// Paragraph element: Copyright - Holberton School
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
