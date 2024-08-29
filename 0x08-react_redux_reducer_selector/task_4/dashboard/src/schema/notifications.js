d28407ba5ee4e1e9be5ad9f43e85737aaef9abfc
RuthWanzila <134962204+RuthWanzila@users.noreply.github.com>
2024-08-29 15:21:15 +0300

    Delete 0x08-react_redux_reducer_selector directory

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++
@@ -1,5 +0,0 @@
-import { schema } from 'normalizr';
-
-const notificationSchema = new schema.Entity('notifications');
-
-export const notificationsNormalizer = (data) => normalize(data, [notificationSchema]);
